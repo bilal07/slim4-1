676a3b92cb6c4dec072b702abaf5dde34431eb95
bilal07 <mouadine.bilal@gmail.com>
2021-03-10 12:43:54 +0100

    add services

--- public/index.php
+++ public/index.php
@@ -3,10 +3,28 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+
+$container = new Container();
+
+$container->set('templating',function(){
+    return new Mustache_Engine([
+        'loader' => new Mustache_Loader_FilesystemLoader(
+            __DIR__. '/../templates',
+            ['extension' => ''] 
+        )
+    ]);
+});
+
+AppFactory::setContainer($container);
+
 $app = AppFactory::create();
+
+
+
 $app->get('/', function(Request $request, Response $response){
     $response->getBody()->write('hello, world !');
     return $response;
@@ -17,8 +35,10 @@ $app->get('/hello/bilal', function(Request $request, Response $response){
     return $response;
 });
 
-$app->get('/hello/{name}', function(Request $request, Response $response, array $arg = []){
-    $response->getBody()->write(sprintf("hello, %s", ucfirst($arg['name'])));
+$app->get('/hello/{name}', function(Request $request, Response $response, array $args = []){
+    //$response->getBody()->write(sprintf("hello, %s", ucfirst($arg['name'])));
+    $html = $this->get('templating')->render('hello.html', ['name' => ucfirst($args['name'])]);
+    $response->getBody()->write($html);
     return $response;
 });
 $app->run();
